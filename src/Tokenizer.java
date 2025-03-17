578990abd1ab4bf90852d553083c75df78672e35
Ray Anthony <62559681+DollieSon@users.noreply.github.com>
2025-03-17 19:57:19 +0800

    Removed the old tokenizer thing

--- src/Tokenizer.java
+++ src/Tokenizer.java
@@ -22,7 +22,10 @@ public class Tokenizer{
         //Last Option
         VARIABLE_NAME,
         //Error
-        ERROR_TOKEN
+        ERROR_TOKEN,
+
+        //Stoppers or Esc
+        COMMA,
     }
     HashMap<String,Token> keyword_pairs;
     public Tokenizer(){
@@ -38,75 +41,6 @@ public class Tokenizer{
         keyword_pairs.put("O",Token.OR_BOOL);
 
     }
-    //This is just shit
-    public Token getToken(String str){
-        //Check for single tokens
-        //not sure if this is an optimized approach btw
-        if(str.length() == 1){ //single token
-            switch (str){
-                case "+":
-                    return Token.ADD_OPP;
-                case "-":
-                    return Token.SUB_OPP;
-                case "*":
-                    return Token.MUL_OPP;
-                case "/":
-                    return Token.DIV_OPP;
-                case "%":
-                    return Token.MOD_OPP;
-            }
-        }
-        //Check for reserved words
-        else{
-            switch (str){
-                case "SUGOD":
-                    return Token.PROGRAM_START;
-                case "KATAPUSAN":
-                    return Token.PROGRAM_END;
-                case "MUGNA":
-                    return Token.VAR_DECLARE;
-            }
-        }
-        //Assume that it's a variable name
-        return Token.VARIABLE_NAME;
-    }
-    //This is unoptimized
-    public ArrayList<Token> getTok(RandomAccessFile line){
-        //Should be an array of an object that holds the string,token, and location
-        ArrayList<Token> res = new ArrayList<>();
-        byte[] ident = new byte[1];
-        try {
-            line.read(ident);
-            String first_ch = new String(ident);
-            //check if it is a single character token
-            switch (first_ch){
-                case "+":
-                    res.add(Token.ADD_OPP);
-                    break;
-                case "-":
-                    res.add(Token.SUB_OPP);
-                    break;
-                case "=":
-                    long marker = line.getFilePointer();
-                    byte[] nextchar = new byte[1];
-                    line.read(nextchar);
-                    if(nextchar.equals("=")){
-                        res.add(Token.EQUALTO_OPP);
-                    }else{
-                        res.add(Token.EQUAL_ASSIGN);
-                        line.seek(marker);
-                    }
-                    //lookahead
-                    break;
-            }
-            //check if it is a reserved keyword
-            //check if a valid variable name
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return res;
-    }
-    // 3rd and final and better version
     public ArrayList<Token> tokenize(Scanner input){
         ArrayList<Token> res = new ArrayList<>();
         while(input.hasNextLine()){
@@ -118,6 +52,7 @@ public class Tokenizer{
                     res.add(this.keyword_pairs.get(word));
                 }else{
                     //parse by else
+
                 }
             }
         }

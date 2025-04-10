5b72ee070e3d79babafc8096571b9dfd606b0479
Ray Anthony <62559681+DollieSon@users.noreply.github.com>
2025-04-10 15:19:12 +0800

    Refactored implementation of complex signs

--- src/Token.java
+++ src/Token.java
@@ -8,6 +8,12 @@ public class Token implements Cloneable {
         this.keyword = str;
         this.line = line;
     }
+
+    public Token setLine(int line) {
+        this.line = line;
+        return this;
+    }
+
     @Override
     public Object clone() throws CloneNotSupportedException{
         return super.clone();
